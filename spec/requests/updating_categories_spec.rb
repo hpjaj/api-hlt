eec645e0713b095cc02d637acf43a333c173778b
hpjaj <hmlevine@gmail.com>
2015-01-04 16:24:24 -0700

    Updating_categories request spec passing with and without auth token.

--- spec/requests/updating_categories_spec.rb
+++ spec/requests/updating_categories_spec.rb
@@ -11,11 +11,24 @@ RSpec.describe 'The categories update method should', :type => :request do
     patch "categories/#{@category.id}",
       { category: { title: 'Favorite Movies' } }.to_json,
       { 'Accept' => Mime::JSON, 'Content-Type' => Mime::JSON.to_s, 'Authorization' => "Token token=#{@user.auth_token}" }
+    expect( response.status ).to eq(204)
 
+    get "/categories/#{@category.id}", {}, {'Accept' => Mime::JSON}
+    expect( response.status ).to eq(200)
+    expect( @category.reload.title ).to eq("Favorite Movies")
+    expect( response.content_type ).to eq(Mime::JSON)
   end
 
   it "should not update an existing category when not provided a valid user auth_token" do
+    patch "categories/#{@category.id}",
+      { category: { title: 'Favorite Movies' } }.to_json,
+      { 'Accept' => Mime::JSON, 'Content-Type' => Mime::JSON.to_s, 'Authorization' => "Token token=#{@user.auth_token}+fake" }
+    expect( response.status ).to eq(401)
 
+    get "/categories/#{@category.id}", {}, {'Accept' => Mime::JSON}
+    expect( response.status ).to eq(200)
+    expect( @category.reload.title ).to eq("Favorite Restaurants")
+    expect( response.content_type ).to eq(Mime::JSON)
   end
 
 end
