ce7e36b1bb93668e02174af980b33e41d3845edb
hpjaj <hmlevine@gmail.com>
2015-01-04 15:49:03 -0700

    Added request spec to test for invalid auth_token when creating a category. Created for jbuilder file for the create method.

--- spec/requests/creating_categories_spec.rb
+++ spec/requests/creating_categories_spec.rb
@@ -18,6 +18,14 @@ RSpec.describe 'The categories create method should', :type => :request do
   end
 
   it "does not create a category without a valid user auth_token" do
+    post '/categories', 
+      {category: 
+        { title: "Movies"}
+      }.to_json,
+      { 'Accept' => Mime::JSON, 'Content-Type' => Mime::JSON.to_s, 'Authorization' => "Token token=#{@user.auth_token}+fake" }
+    expect( response.content_type ).to eq(Mime::JSON)
+    expect( response.status ).to eq(401)
+    expect( response.body ).to_not include("Movies")
   end
 
 end
